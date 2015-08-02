650233d03125dc1df531d75207275ea407a34aa5
ChaosLeong <lgf42031@gmail.com>
2015-08-03 01:16:04 +0800

    添加 Android Studio 打 Jar 包 Simple

---
+++ AndroidJarDemo/src/main/java/org/chaos/demo/jar/MyClass.java
@@ -0,0 +1,13 @@
+package org.chaos.demo.jar;
+
+import android.util.Log;
+
+/**
+ * @author Chaos
+ *         2015/06/05.
+ */
+public class MyClass {
+    public static void logD(String msg) {
+        Log.e("TAG", msg);
+    }
+}
---
+++ AndroidJarDemo/src/main/java/org/chaos/demo/jar/MyClass2.java
@@ -0,0 +1,8 @@
+package org.chaos.demo.jar;
+
+/**
+ * @author Chaos
+ *         2015/06/05.
+ */
+public class MyClass2 {
+}
